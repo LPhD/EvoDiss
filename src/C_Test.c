0c5d8bc791055798ec6711f65811ea04314166a9
Lea <gerling@sse.uni-hildesheim.de>
2017-08-07 10:53:21 +0200

    Refactoring

--- src/C_Test.c
+++ src/C_Test.c
@@ -13,10 +13,8 @@ void compareResults(int *arraySorted, int *arrayExpected, int length){
 	}
 }
 
-void mainTest (){
-	//ToDo work with return of compare method
-	//int tester = 0;
-
+//Runs tests with an array with three elements
+void threeElmArray(){
 	int arrayTest_1[3] = { 3,2,1 };
 	int arrayTest_1_sortedASC[3] = { 1,2,3 };
 	int arrayTest_1_sortedDESC[3] = { 3,2,1 };
@@ -24,7 +22,10 @@ void mainTest (){
 	compareResults(arrayTest_1, arrayTest_1_sortedASC, 3);
 	bubblesortReversed(arrayTest_1, 3);
 	compareResults(arrayTest_1, arrayTest_1_sortedDESC, 3);
+}
 
+//Runs tests with an empty array
+void zeroElmArray(){
 	int arrayTest_2[0] = {};
 	int arrayTest_2_sortedASC[0] = {};
 	int arrayTest_2_sortedDESC[0] = {};
@@ -32,8 +33,10 @@ void mainTest (){
 	compareResults(arrayTest_2, arrayTest_2_sortedASC, 0);
 	bubblesortReversed(arrayTest_2, 0);
 	compareResults(arrayTest_2, arrayTest_2_sortedDESC, 0);
+}
 
-
+//Runs tests with an array with ten elements
+void tenElmArray(){
 	int arrayTest_3[10] = {7, 3, 12, 3, 4, 1, 9, 3, 0, -1};
 	int arrayTest_3_sortedASC[10] = {-1, 0 , 1, 3, 3, 3, 4, 7, 9, 12};
 	int arrayTest_3_sortedDESC[10] = {12, 9, 7, 4, 3, 3, 3, 1, 0, -1};
@@ -41,4 +44,13 @@ void mainTest (){
 	compareResults(arrayTest_3, arrayTest_3_sortedASC, 10);
 	bubblesortReversed(arrayTest_3, 10);
 	compareResults(arrayTest_3, arrayTest_3_sortedDESC, 10);
+}
+
+void mainTest (){
+	//ToDo work with return of compare method
+	//int tester = 0;
+
+	threeElmArray();
+	zeroElmArray();
+	tenElmArray();
 }
