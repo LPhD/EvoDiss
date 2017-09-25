026ccb59c6aaa03b97a7f902b94019c4c66ca0fe
Lea <Lea@DESKTOP-G5CE7DO>
2017-09-25 18:01:13 +0200

    Added testcases for pure::variants

--- src/Macros.c
+++ src/Macros.c
@@ -10,6 +10,38 @@
 #define BUBBLE2 BUBBLE
 
 
+//Test method for macros and partial preprocessor in pure::variants
+void test (){
+
+//True for Selection of Feature_A
+#if (defined(Feature_A) && (MAKRO(7) > 7))
+puts("Feature A and Macro");
+#endif
+
+//True for Selection of Feature_B
+#if (defined(Feature_B) && (MAKRO(7) > 7))
+puts("Feature B and Macro");
+#endif
+
+//Never true
+#if (defined(Feature_A) && !(MAKRO(7) > 7))
+puts("Feature A and not Macro");
+#endif
+
+//Always true
+#if (defined(Feature_A) || (MAKRO(7) > 7))
+puts("Feature A or Macro");
+#endif
+
+//True for Selection of Feature_A
+#if defined(Feature_A)
+puts("Feature A");
+#endif
+
+
+}
+
+
 void testMacros(){
 
 #if(MAKRO(7) > 7) \
@@ -31,4 +63,6 @@ printf("\nMakro MAKRO(7): %i",MAKRO(7));
 #ifdef BUBBLE2
 	puts("\nBubble selected!");
 #endif
+
+	test();
 }
